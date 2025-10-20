f012bca4bab7eea00e6c201b14d37981fd7506b0
Giorgi Kuchava <giorgikuchava@Giorgis-MacBook-Pro.local>
2025-10-20 23:27:21 +0400

    My elementary code for day1

---
+++ src/test/java/CommandsTest.java
@@ -0,0 +1,12 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.safari.SafariDriver;
+
+public class CommandsTest {
+    public static void main(String[] args) throws InterruptedException {
+
+      System.out.println("Hello World!");
+
+
+    }
+}
